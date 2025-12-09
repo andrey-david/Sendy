a3fdc730a9cb8015311c3bd3839815ddf591fc64
andrey-david <andrey.david.qwerty@gmail.com>
2025-12-09 18:22:26 +0300

    refactored image loader

--- image_loader/image_loader.py
+++ image_loader/image_loader.py
@@ -13,8 +13,9 @@ Usage:
     from image_loader import image_loader
     await image_loader()
 """
+
 import asyncio
-import os
+from pathlib import Path
 import logging
 
 from watchfiles import awatch
@@ -24,32 +25,44 @@ from aiogram import Router
 
 from data import data
 from config import config
-from lexicon import image_loader_error_text
+from lexicon import image_loader_error_text, image_loader_lex
 
 logger = logging.getLogger(__name__)
 image_loader_router = Router(name='image_loader_router')
 
 
 async def image_loader() -> None:
-    path = data.image_loader_path
-    uploaded_path = os.path.join(path, 'Uploaded')
+    path: Path = data.image_loader_path
+    uploaded_path: Path = path / 'Uploaded'
+    allowed_suffix = (".jpg", ".png", ".heic")
 
-    if os.path.exists(path):
-        os.makedirs(uploaded_path, exist_ok=True)
+    if path.exists():
+        uploaded_path.mkdir(exist_ok=True)
         async for _ in awatch(path):
-            for file_name in os.listdir(path):
-                if file_name.lower().endswith((".jpg", ".png", ".heic")):
-                    file_path = os.path.join(path, file_name)
+            for file_name in path.glob('*'):
+                if file_name.suffix.lower() in allowed_suffix:
+                    file_path = path / file_name
 
                     try:
-                        if os.path.exists(file_path):
-                            await config.bot.bot.send_document(chat_id=config.bot.chat_id, document=FSInputFile(file_path))
-                            os.replace(file_path, os.path.join(uploaded_path, file_name))
+                        size_mb = file_path.stat().st_size / (1024 * 1024)
+                        logger.debug(f'File name: {file_path.name}, File size: {size_mb}')
+                        if size_mb >= 50:
+                            raise Exception
+
+                        msg = await config.bot.bot.send_message(chat_id=config.bot.chat_id, text=image_loader_lex['sending_file'])
+                        await config.bot.bot.send_document(chat_id=config.bot.chat_id, document=FSInputFile(file_path))
+                        await msg.delete()
+
+                        dst = uploaded_path / file_path.name
+                        file_path.rename(dst)
+                        await asyncio.sleep(1)
+
                     except TelegramBadRequest:
                         logger.exception('Corrupted file or Wrong chat ID')
-
-                    await asyncio.sleep(1)
+                        await config.bot.bot.send_message(chat_id=config.bot.chat_id, text=image_loader_lex['error_corrupted_file'])
+                    except Exception:
+                        logger.exception('Cannot send image')
 
     else:
-        await config.bot.bot.send_message(chat_id=config.bot.chat_id, text=image_loader_error_text())
         logger.error('Invalid Image loader path')
+        await config.bot.bot.send_message(chat_id=config.bot.chat_id, text=image_loader_error_text())
